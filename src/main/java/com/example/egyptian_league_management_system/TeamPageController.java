3cb877d74555fbc2a2db9918102527759b08da25
omarabo3sh <159817699+omarabo3sh@users.noreply.github.com>
2024-06-21 16:17:43 +0300

    ABOESH

---
+++ src/main/java/com/example/egyptian_league_management_system/TeamPageController.java
@@ -0,0 +1,8 @@
+package com.example.egyptian_league_management_system;
+
+import javafx.event.ActionEvent;
+
+public class TeamPageController {
+    public void onBackClick(ActionEvent actionEvent) {
+    }
+}
