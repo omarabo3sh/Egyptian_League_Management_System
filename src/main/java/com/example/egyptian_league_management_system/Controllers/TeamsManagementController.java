7c1d32f648d7487096a8d67cba63fa035685cb96
omarabo3sh <159817699+omarabo3sh@users.noreply.github.com>
2024-06-26 00:12:14 +0300

    aboesh

--- src/main/java/com/example/egyptian_league_management_system/Controllers/TeamsManagementController.java
+++ src/main/java/com/example/egyptian_league_management_system/Controllers/TeamsManagementController.java
@@ -2,33 +2,42 @@ package com.example.egyptian_league_management_system.Controllers;
 
 import com.example.egyptian_league_management_system.Application;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 import java.io.IOException;
 
 public class TeamsManagementController {
-    public void onBackClick(ActionEvent event) throws IOException {
-        Application.switchScene(event, "choose.fxml");
+
+    @FXML
+    private Label infoLabel;
+
+
+
+    public void onDisplayTeamInformationClick(ActionEvent event) {
+        String data="";
+        infoLabel.setText(data);
     }
 
-    public void onEnterTeamInformationClick(ActionEvent event)  throws IOException {
-        Application.switchScene(event,"enterTeamInformation.fxml");
-
+    public void onDisplayTeamMatchesClick(ActionEvent event) { String data="";
+        infoLabel.setText(data);
     }
 
-    public void onUpdateTeamInformationClick(ActionEvent event)  throws IOException {
-        Application.switchScene(event,"updateTeamInformation.fxml");
-
+    public void onDisplayTeamDetailedScoresClick(ActionEvent event) { String data="";
+        infoLabel.setText(data);
     }
-    public void onDisplayTeamInformationClick(ActionEvent event)  throws IOException {
-        Application.switchScene(event,"displayTeamInformation.fxml");
 
-    }
-    public void onDisplayTeamIMatchesClick(ActionEvent event)  throws IOException {
-        Application.switchScene(event,"displayTeamMatches.fxml");
 
-    }
-    public void onDisplayTeamDetailedScoresClick(ActionEvent event)  throws IOException {
-        Application.switchScene(event,"displayTeamDetailedScores.fxml");
-
-    }
+public void onBackClick(ActionEvent event) throws IOException, IOException {
+    Application.switchScene(event, "choose.fxml");
 }
+
+public void onEnterTeamInformationClick(ActionEvent event)  throws IOException {
+    Application.switchScene(event,"enterTeamInformation.fxml");
+
+}
+
+public void onUpdateTeamInformationClick(ActionEvent event) throws IOException, IOException {
+    Application.switchScene(event,"updateTeamInformation.fxml");
+
+}}
