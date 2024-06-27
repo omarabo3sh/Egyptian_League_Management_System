a80a828ae4c24022d70598ba357b068f08e80f6c
omarabo3sh <159817699+omarabo3sh@users.noreply.github.com>
2024-06-27 22:03:01 +0300

    aboesh

--- src/main/java/com/example/egyptian_league_management_system/Controllers/MatchesManagementController.java
+++ src/main/java/com/example/egyptian_league_management_system/Controllers/MatchesManagementController.java
@@ -1,4 +1,8 @@
 package com.example.egyptian_league_management_system.Controllers;
 
+import javafx.event.ActionEvent;
+
 public class MatchesManagementController {
+    public void onBackClick(ActionEvent event) {
+    }
 }
